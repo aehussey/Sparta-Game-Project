8ae177c771f0fc7ca41f38de3e8cd01680bb2c40
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-15 16:37:24 +0100

    Dealt with the dealing of the cards and turnover of the main deck

--- js/main.js
+++ js/main.js
@@ -2,6 +2,45 @@ $(function () {
 
 console.log("hello");
 
+
+
+function drawStarterCardsLeft(createStartMove) {
+  for (var i = 0; i < createStartMove.length; i++) {
+
+    $($(".solitairePileLeft")[i]).append('<img src="images/card-images/' + createStartMove[i] + '.png">');
+
+  }
+}
+
+function drawStarterCardsRight(createStartMove) {
+  for (var i = 0; i < createStartMove.length; i++) {
+    $($(".solitairePileRight")[i]).append('<img src="images/card-images/' + createStartMove[i] + '.png">');
+
+  }
+}
+
+function drawFirstCard(card1, card2) {
+  $("#player1FaceUp").append('<img src="images/card-images/' + card1 + '.png">');
+  $("#player2FaceUp").append('<img src="images/card-images/' + card2 + '.png">');
+}
+
+function drawCardPlayer1(card) {
+  $("#player1FaceUp").append('<img src="images/card-images/' + card + '.png">');
+  $("#player1FaceUp img").addClass("cardPile");
+
+}
+
+function drawCardPlayer2(card) {
+  $("#player2FaceUp").append('<img src="images/card-images/' + card + '.png">');
+  $("#player2FaceUp img").addClass("cardPile");
+
+}
+
+function player1Talon(card) {
+  $("#player1Talon").append('<img src="images/card-images/' + card + '.png">');
+  $("player1Talon img").addClass("cardPile");
+}
+
 class Deck {
   constructor() {
     this.deck = [];
@@ -11,7 +50,7 @@ class Deck {
 
     for (var suit in suits) {
       for (var value in values) {
-        this.deck.push(`${values[value]} of ${suits[suit]}`);
+        this.deck.push(`${values[value]}-of-${suits[suit]}`);
       }
     }
   }
@@ -36,6 +75,11 @@ class Deck {
   return talonArray;
 }
 
+  drawCardTalon () {
+    return talonArray.pop();
+    console.log(talonArray.pop());
+  }
+
   createStartMove () {
     var startMoveArray = [];
     for (var i = 0; i < 4; i++) {
@@ -53,20 +97,55 @@ class Deck {
 
 
 
-$("#startGame").click(function(){
-  console.log("start game");
-  const deck1 = new Deck();
-  deck1.shuffle();
-  deck1.createStartMove()
-  console.log(deck1.createTalon());
-  console.log(deck1.deck);
-  // console.log(deck1.createTalon());
-  const deck2 = new Deck();
-  deck2.shuffle();
-  deck2.createStartMove()
-  console.log(deck2.createTalon());
-  console.log(deck2.deck);
-  // console.log(deck2.createTalon());
+  $("#startGame").click(function(){
+
+    deck1 = new Deck();
+    deck1.shuffle();
+
+    deck2 = new Deck();
+    deck2.shuffle();
+
+    drawFirstCard (deck1.drawCard(), deck2.drawCard())
+
+    drawStarterCardsLeft(deck1.createStartMove())
+    console.log(deck1.createTalon());
+    console.log(deck1.deck);
+
+
+    drawStarterCardsRight(deck2.createStartMove())
+    console.log(deck2.createTalon());
+    console.log(deck2.deck);
+
+
+    $("#startGame").hide();
+
+
+
+  })
+
+
+  $("#player1FaceUp").click(function () {
+    drawCardPlayer1(deck1.drawCard())
+    console.log(deck1.deck);
+  })
+
+  $("#player2FaceUp").click(function () {
+    drawCardPlayer2(deck2.drawCard())
+    console.log(deck2.deck);
+  })
+
+
+$("#player1Talon").click(function () {
+  player1Talon(deck1.createTalon())
 
 })
+
+$("#endTurn").click(function () {
+  console.log("End Turn");
+  turn == "X" ? turn = "Y" : turn = "X"
+
+})
+
+
+
 });
